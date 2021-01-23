26055eeb3548fe69fbdfa5f442a21c7a882bb7f5
le-michael <michael.le647@gmail.com>
2021-01-22 19:15:12 -0500

    Add ip address text box

--- demos/testserver/MainMenu.gd
+++ demos/testserver/MainMenu.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 signal server_start
 signal client_start
 
+var ip = ""
+
 func _ready():
 	pass # Replace with function body.
 
@@ -13,3 +15,8 @@ func _on_ServerButton_pressed():
 func _on_StartClient_pressed():
 	Server.init_client()
 	get_tree().change_scene("res://World.tscn")
+
+
+func _on_TextEdit_text_changed():
+	print ($TextEdit.text)
+	Server.ip = $TextEdit.text
